cef7c368b6ce2125eb1f90d6cee7235c41c98192
helpsdan <daniel_aguiar_alves@hotmail.com>
2018-06-24 19:33:35 -0300

    list

--- app/src/main/java/br/com/almacel/incidentes/activities/ListarIncidentesActivity.java
+++ app/src/main/java/br/com/almacel/incidentes/activities/ListarIncidentesActivity.java
@@ -1,10 +1,11 @@
 package br.com.almacel.incidentes.activities;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.ListView;
 
-import br.com.almacel.incidentes.R;
+        import br.com.almacel.incidentes.R;
 
 /**
  * @author Daniel Aguiar
@@ -12,10 +13,14 @@ import br.com.almacel.incidentes.R;
  */
 public class ListarIncidentesActivity extends AppCompatActivity implements View.OnClickListener{
 
+    private ListView lista;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listar_incidentes);
+
+
     }
     @Override
     public void onClick(View v) {
